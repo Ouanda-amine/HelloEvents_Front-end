8fee939e9fa712408008d51c7191b9a01077f94c
amiine <amineouanda@gmail.com>
2025-05-28 14:39:14 +0100

    EventService : func = getALlEvents

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatButton} from '@angular/material/button';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    MatToolbar,
+    MatButton
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
